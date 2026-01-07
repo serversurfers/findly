b2d8dbd62bffcc3f1fa3af6c76d480a1c3844858
Yves-Developer <yvesmugisha09@gmail.com>
2026-01-07 17:05:17 +0200

    shadcn initiliastion

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 import Image from "next/image";
 
 export default function Home() {
@@ -29,24 +34,58 @@ export default function Home() {
           </span>
         </h2>
 
-        <p className="mt-6 max-w-xl text-lg text-zinc-600 dark:text-zinc-400">
+        <p className="mt-6 max-w-xl text-lg text-white">
           Findly is a university-focused lost and found platform that helps
           students and staff report, search, and recover lost items easily.
         </p>
 
         <div className="mt-10 flex flex-col gap-4 sm:flex-row">
-          <a
+          {/* <a
             href="/lost"
             className="rounded-full bg-black px-8 py-3 text-white transition hover:bg-zinc-800 dark:bg-white dark:text-black dark:hover:bg-zinc-200"
           >
             Report Lost Item
-          </a>
-          <a
+          </a> */}
+          <Button size={'lg'} className="py-6">Report Lost Item</Button>
+          {/* <a
             href="/found"
             className="rounded-full border border-black px-8 py-3 transition hover:bg-black hover:text-white dark:border-white dark:hover:bg-white dark:hover:text-black"
           >
             Browse Found Items (Updated)
-          </a>
+          </a> */}
+              <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="outline" size={'lg'} className="py-6">Open Dialog</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="username-1">Username</Label>
+              <Input id="username-1" name="username" defaultValue="@peduarte" />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+          <Button variant={'outline'} size={'lg'} className="py-6">Browse Found Items (Updated)</Button>
         </div>
       </main>
 
