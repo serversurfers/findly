09b510678f296e41b24b61d18a3daaf8146b80b8
Yves-Developer <yvesmugisha09@gmail.com>
2026-01-15 22:06:17 +0200

    shadcn select commponent

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,27 @@ import { Heading } from "@/components/custom/header";
 import { NavLink } from "@/components/custom/heading_links";
 import { Logo } from "@/components/custom/logo";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 import Image from "next/image";
 
@@ -12,7 +30,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white text-black dark:bg-black dark:text-white">
       {/* Header */}
-      <Heading/>
+      <Heading />
       {/* Hero Section */}
       <main className="mx-auto flex max-w-6xl flex-col items-center px-6 py-32 text-center">
         <h2 className="max-w-3xl text-4xl font-semibold leading-tight tracking-tight sm:text-5xl">
@@ -23,6 +41,24 @@ export default function Home() {
           </span>
         </h2>
 
+         {/* SELECT */}
+      <Select>
+        {/* Trigger */}
+        <SelectTrigger>
+          <SelectValue placeholder="Select Schools" />
+        </SelectTrigger>
+        {/* Select Content */}
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Universities</SelectLabel>
+            <SelectItem value="auca">AUCA</SelectItem>
+            <SelectItem value="auca2">AUCA1</SelectItem>
+            <SelectLabel>Highschool</SelectLabel>
+            <SelectItem value="auca3">AUCA2</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+
         <div className="mt-10 flex flex-col gap-4 sm:flex-row">
           {/* <a
             href="/lost"
@@ -30,52 +66,68 @@ export default function Home() {
           >
             Report Lost Item
           </a> */}
-          <Button size={'lg'} className="py-6">Report Lost Item</Button>
+          <Button size={"lg"} className="py-6">
+            Report Lost Item
+          </Button>
           {/* <a
             href="/found"
             className="rounded-full border border-black px-8 py-3 transition hover:bg-black hover:text-white dark:border-white dark:hover:bg-white dark:hover:text-black"
           >
             Browse Found Items (Updated)
           </a> */}
-              <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button variant="outline" size={'lg'} className="py-6">Open Dialog</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4">
-            <div className="grid gap-3">
-              <Label htmlFor="name-1">Name</Label>
-              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="username-1">Username</Label>
-              <Input id="username-1" name="username" defaultValue="@peduarte" />
-            </div>
-          </div>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </form>
-    </Dialog>
-          <Button variant={'outline'} size={'lg'} className="py-6">Browse Found Items (Updated)</Button>
+          <Dialog>
+            <form>
+              <DialogTrigger asChild>
+                <Button variant="outline" size={"lg"} className="py-6">
+                  Open Dialog
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Edit profile</DialogTitle>
+                  <DialogDescription>
+                    Make changes to your profile here. Click save when
+                    you&apos;re done.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4">
+                  <div className="grid gap-3">
+                    <Label htmlFor="name-1">Name</Label>
+                    <Input
+                      id="name-1"
+                      name="name"
+                      defaultValue="Pedro Duarte"
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="username-1">Username</Label>
+                    <Input
+                      id="username-1"
+                      name="username"
+                      defaultValue="@peduarte"
+                    />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant="outline">Cancel</Button>
+                  </DialogClose>
+                  <Button type="submit">Save changes</Button>
+                </DialogFooter>
+              </DialogContent>
+            </form>
+          </Dialog>
+          <Button variant={"outline"} size={"lg"} className="py-6">
+            Browse Found Items (Updated)
+          </Button>
         </div>
       </main>
 
+     
       {/* Footer */}
       <footer className="border-t border-zinc-200 px-6 py-6 text-center text-sm text-zinc-500 dark:border-zinc-800 dark:text-zinc-400">
-        © {new Date().getFullYear()} Findly · Built for university communities By TheServerSurfers.
+        © {new Date().getFullYear()} Findly · Built for university communities
+        By TheServerSurfers.
       </footer>
     </div>
   );
